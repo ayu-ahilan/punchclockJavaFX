aeec99933c230b3590c9fa915614874322260afb
ayu-ahilan <ahilayu@hotmail.com>
2021-09-30 08:56:58 +0200

    Buttons bei auf Main-Seite gemacht

--- src/main/java/ch/zli/aa/punchclockjavafx/punchclockjavafx/MainController.java
+++ src/main/java/ch/zli/aa/punchclockjavafx/punchclockjavafx/MainController.java
@@ -3,12 +3,25 @@ package ch.zli.aa.punchclockjavafx.punchclockjavafx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class MainController {
-    @FXML
-    private Label welcomeText;
+import java.io.IOException;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class MainController {
+    public void saveData() throws IOException {
+
     }
+
+    public void logout() throws IOException {
+
+        HelloApplication.getInstance().toLogin_view();
+    }
+
+    public void delete() throws IOException {
+
+    }
+
+    public void admin() throws IOException {
+
+        HelloApplication.getInstance().toAdmin_view();
+    }
+
 }
